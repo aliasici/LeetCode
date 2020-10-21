97c8beb564ea409abbe14f87fc3fbe2055b34785
aliasici <asiciali@yahoo.com>
2020-10-21 14:19:57 -0700

    first commit

---
+++ src/SelfPractices/ReverseString.java
@@ -0,0 +1,15 @@
+package SelfPractices;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "ali";
+        String reversed = "";
+
+        for(int i = str.length()-1; i >= 0; i--){
+
+            reversed += str.charAt(i);
+        }
+        System.out.println(reversed);
+
+    }
+}
